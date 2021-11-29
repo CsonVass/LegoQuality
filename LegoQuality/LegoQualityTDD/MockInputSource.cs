e15990b6689a92093066d4c1706dcb16453344cd
hoppare <hoppare@sch.bme.hu>
2021-11-29 17:33:16 +0100

    inputsource tdd ready

---
+++ LegoQuality/LegoQualityTDD/MockInputSource.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LegoQualityTDD
+{
+    public class MockInputSource
+    {
+        private List<String> mockInputLines;
+ 
+
+        public MockInputSource(String mockInputFilePath)
+        {
+            this.mockInputLines = new List<string>(File.ReadAllLines(mockInputFilePath)).ToList();
+        }
+        public string GetNextInput()
+        {
+            if (this.HasNextInput())
+            {
+                String input = mockInputLines[0];
+                mockInputLines.RemoveAt(0);
+                return input;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+
+        public bool HasNextInput()
+        {
+            return mockInputLines.Count > 0;
+        }
+    }
+}
---
+++ LegoQuality/Tests/TestMockInputSource.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LegoQualityTDD;
+using System.IO;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestMockInputSource
+    {
+
+        string emptyInput = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, "testinput_empty.txt");
+        string testInput1 = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, "testinput1.txt");
+
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void OpenFileNotFound()
+        {
+            MockInputSource mis = new MockInputSource("not_existing_file.txt");
+        }
+
+        [TestMethod]
+        public void EmptyFileHasNextInput()
+        {
+            MockInputSource mis = new MockInputSource(emptyInput);
+            Assert.IsFalse(mis.HasNextInput());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void EmptyFileGetNextInput()
+        {
+            MockInputSource mis = new MockInputSource(emptyInput);
+            mis.GetNextInput();
+        }
+
+        [TestMethod]
+        public void GetNextInputWithoutCheckingIfHasNextInput()
+        {
+            MockInputSource mis = new MockInputSource(testInput1);
+            Assert.AreEqual("1637601466;2;0;red;0;0;1", mis.GetNextInput());
+        }
+
+        [TestMethod]
+        public void GetNextInputWithCheckingIfHasNextInput()
+        {
+            MockInputSource mis = new MockInputSource(testInput1);
+            Assert.IsTrue(mis.HasNextInput());
+            Assert.AreEqual("1637601466;2;0;red;0;0;1", mis.GetNextInput());
+        }
+
+        [TestMethod]
+        public void CheckNumberOfInputsIsCorrect()
+        {
+            MockInputSource mis = new MockInputSource(testInput1);
+            int count = 0;
+            while (mis.HasNextInput())
+            {
+                string foo = mis.GetNextInput();
+                count++;
+            }
+            Assert.AreEqual(3, count);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void CheckIfExceptionIsThrownInEndlessLoop()
+        {
+            MockInputSource mis = new MockInputSource(testInput1);
+            while (true)
+            {
+                string foo = mis.GetNextInput();
+            }
+        }
+
+
+
+
+    }
+}
